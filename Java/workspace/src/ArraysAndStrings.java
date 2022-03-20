0bb6be8a897cd178d0f1e6f0c65ae47eb732970c
Aleksandar Milovanović <59347685+aleksannndar@users.noreply.github.com>
2022-03-20 03:54:16 +0100

    Finished Arrays and Strings for Java

--- Java/workspace/src/ArraysAndStrings.java
+++ Java/workspace/src/ArraysAndStrings.java
@@ -163,6 +163,185 @@ public class ArraysAndStrings {
         return true;
     }
 
-    
+    // space O(1), time O(n)
+    static boolean solution5a(String first, String second){
+        if(Math.abs(first.length() - second.length()) > 1)
+            return false;
+
+        String longer = first.length() > second.length() ? first : second;
+        String shorter = first.length() > second.length() ? second : first;
+
+        int indexLonger = 0;
+        int indexShorter = 0;
+        boolean oneChange = false;
+
+        while(indexShorter < shorter.length() && indexLonger < longer.length()){
+            if(longer.charAt(indexLonger) != shorter.charAt(indexShorter)){
+                if(oneChange)
+                    return false;
+                oneChange = true;
+
+                if(shorter.length() == longer.length()){
+                    indexShorter++;
+                }
+            }else{
+                indexShorter++;
+            }
+            indexLonger++;
+        }
+
+        return true;
+    }
+
+    //space O(1), time O(n)
+    static String solution6a(String input){
+        if(input.length() <= 2)
+            return input;
+
+        StringBuilder sb = new StringBuilder();
+
+        int occurancesCounter = 1;
+        for(int i = 1; i < input.length(); i++){
+            if(input.charAt(i) != input.charAt(i - 1)){
+                sb.append(input.charAt(i-1));
+                sb.append(occurancesCounter);
+                occurancesCounter = 1;
+            }else{
+                occurancesCounter++;
+            }
+        }
+
+        sb.append(input.charAt(input.length()-1));
+        sb.append(occurancesCounter);
+
+        if(sb.length() >= input.length())
+            return input;
+        else
+            return sb.toString();
+    }
+
+    //space O(1), time O(n^2)
+    static boolean solution7a(int[][] input){
+        if(input.length == 0 || input.length != input[0].length)
+            return false;
+
+        int n = input.length;
+
+        for(int i = 0; i < n / 2; i ++){
+            int last = n - 1 - i;
+            for(int j = i; j < last; j++){
+                int top = input[i][j];
+
+                //left -> top
+                input[i][j] = input[last-j+i][i];
+
+                //bot -> left
+                input[last - j + i ][i] = input[last][last-j + i];
+
+                //right -> bot
+                input[last][last - j + i] = input[j][last];
+
+                //top -> right
+                input[j][last] = top;
+            }
+        }
+        return true;
+    }
+
+    //space O(n), time O(n^2)
+    static void solution8a(int[][] input){
+        if(input == null || input.length == 0 || input[0].length == 0)
+            return;
+
+        boolean[] rowHasZero = new boolean[input.length];
+        boolean[] columnHasZero = new boolean[input[0].length];
+
+        for(int i = 0; i < input.length; i++){
+            for(int j = 0; j < input[0].length; j++){
+                if(input[i][j] == 0){
+                    rowHasZero[i] = true;
+                    columnHasZero[j] = true;
+                }
+            }
+        }
+
+        for(int i = 0; i < input.length; i++){
+            for(int j = 0; j < input[0].length; j++){
+                if(rowHasZero[i] || columnHasZero[j])
+                    input[i][j] = 0;
+            }
+        }
+    }
+
+
+    //space O(1), time O(n^2)
+    static void solution8b(int[][] input){
+        if(input == null || input.length == 0 || input[0].length == 0)
+            return;
+
+        boolean columnHasZero = false;
+        for(int i = 0; i < input.length; i++){
+            if(input[i][0] == 0) {
+                columnHasZero = true;
+                break;
+            }
+        }
+
+        boolean rowHasZero = false;
+        for(int i = 0; i < input[0].length; i++){
+            if(input[0][i] == 0){
+                rowHasZero = true;
+                break;
+            }
+        }
+
+        for(int i = 1; i < input.length; i++){
+            for(int j = 1; j < input[0].length; j++){
+                if(input[i][j] == 0){
+                    input[i][0] = 0;
+                    input[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i = 1; i < input.length; i++){
+            for(int j = 1; j < input[0].length; j++){
+                if(input[i][0] == 0 || input[0][j] == 0)
+                    input[i][j] = 0;
+            }
+        }
+
+        if(columnHasZero){
+            for(int i = 0; i < input.length; i++){
+                input[i][0] = 0;
+            }
+        }
+
+        if(rowHasZero){
+            for(int i = 0; i < input[0].length; i++){
+                input[0][i] = 0;
+            }
+        }
+    }
+
+    //dummy function, checks if s2 is substring of s1
+    static boolean isSubstring(String s1, String s2){
+        return true;
+    }
+
+    //space O(1), time depends on isSubstring
+    static boolean solution9a(String s1, String s2){
+        if(s1.isEmpty() || s2.isEmpty())
+            return false;
+
+        StringBuilder sb = new StringBuilder(s2.length() * 2);
+        sb.append(s2);
+        sb.append(s2);
+        s2 = sb.toString();
+
+        return isSubstring(s2, s1);
+    }
+
+
 
 }
--- Java/workspace/src/Main.java
+++ Java/workspace/src/Main.java
@@ -1,5 +1,33 @@
 public class Main {
     public static void main(String[] args){
-        System.out.println(ArraysAndStrings.solution4b("Tact coa"));
+        int[][] matrix = new int[6][6];
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 6; j++){
+                matrix[i][j] = i + j;
+            }
+        }
+
+        matrix[0][0] = 2;
+        matrix[0][5] = 0;
+        matrix[3][4] = 0;
+
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 6; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        ArraysAndStrings.solution8b(matrix);
+
+        for(int i = 0; i < 6; i++){
+            for(int j = 0; j < 6; j++){
+                System.out.print(matrix[i][j] +" ");
+            }
+            System.out.println();
+        }
     }
 }
